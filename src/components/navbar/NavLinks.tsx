b74525b2c24cf7d7ec211790007d4f47954e1919
rowleks <momohrowland@gmail.com>
2025-05-08 12:54:40 +0100

    completed navbar

---
+++ src/components/navbar/NavLinks.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import Facebook from "../socials/Facebook";
+import Instagram from "../socials/Instagram";
+import Twitter from "../socials/Twitter";
+import Whatsapp from "../socials/Whatsapp";
+
+export default function NavLinks() {
+  return (
+    <div className="bg-pink h-[30rem] flex justify-center items-center relative z-10 max-lg:h-screen border-4 border-t-0 rounded-b-[2rem] ">
+      <nav className="flex">
+        <ul className="flex flex-col lg:flex-row gap-8 max-lg:items-center">
+          <Link href="/" className="animate-link">
+            <h1>Home</h1>
+          </Link>
+          <Link href="#" className="animate-link">
+            <h1>Menu</h1>
+          </Link>
+          <Link href="#" className="animate-link">
+            <h1>Categories</h1>
+          </Link>
+          <Link href="#" className="animate-link">
+            <h1>Contact</h1>
+          </Link>
+        </ul>
+
+        <div className="absolute bottom-[10%] left-0 z-50 w-full">
+          <div className="flex items-center justify-center gap-5">
+            <Facebook />
+            <Instagram />
+            <Twitter />
+            <Whatsapp />
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+}
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,7 +1,101 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import NavLinks from "./NavLinks";
+
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev);
+  };
+
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
+    <header className="relative py-6 md:py-5">
+      <div className="px-5 md:px-10">
+        <div className="max-w-[90rem] mx-auto">
+          <div className="flex items-center justify-between gap-15">
+            <Link href="/" className="cursor-pointer relative z-20">
+              <Image
+                src="/images/nords-logo-black.png"
+                alt="nords logo"
+                width={125}
+                height={40}
+              />
+            </Link>
+
+            <div className="flex items-center gap-4 relative z-30">
+              <div className="relative min-w-11 cursor-pointer">
+                <Image
+                  src="/icons/cart-icon.svg"
+                  alt="cart icon"
+                  width={24}
+                  height={24}
+                />
+                <div
+                  className={`cart-quantity ${
+                    isMenuOpen ? "border-pink" : "border-yellow"
+                  } `}
+                >
+                  <span>0</span>
+                </div>
+              </div>
+
+              <div
+                aria-label="menu"
+                role="button"
+                aria-expanded={isMenuOpen}
+                className="btn py-2 px-3 md:py-2.5 overflow-clip"
+                onClick={toggleMenu}
+              >
+                <div className="flex items-center gap-3">
+                  <span className="text-sm md:text-lg font-semibold">MENU</span>
+                  <div className="flex flex-col gap-[1.5px] md:gap-[0.9px] w-9 h-9">
+                    <Image
+                      src="/icons/burger-top.svg"
+                      alt="hamburger menu"
+                      width={38}
+                      height={18}
+                      className={`transition-all duration-300 ${
+                        isMenuOpen ? "top-open" : "close"
+                      }`}
+                    />
+                    <Image
+                      src="/icons/burger-middle.svg"
+                      alt="hamburger menu"
+                      width={38}
+                      height={18}
+                      className={`transition-all duration-700 ease-out relative ${
+                        isMenuOpen ? "mid-open" : "close"
+                      }`}
+                    />
+                    <Image
+                      src="/icons/burger-bottom.svg"
+                      alt="hamburger menu"
+                      width={38}
+                      height={18}
+                      className={`transition-all duration-300 ${
+                        isMenuOpen ? "bottom-open" : "close"
+                      }`}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* Sliding NavLinks */}
+      <div
+        className={`absolute top-0 left-0 w-full transition-transform duration-700 delay-500 ${
+          isMenuOpen ? "translate-y-0" : "-translate-y-full"
+        }`}
+      >
+        <NavLinks />
+      </div>
+    </header>
   );
 }
